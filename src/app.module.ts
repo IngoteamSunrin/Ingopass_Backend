93fefc9f07bbcc5620190718a995956d46a40b3d
fxrcha <truetype24@gmail.com>
2022-09-17 03:35:41 +0900

    chore(app): mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,25 @@ import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { UserModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     AuthModule,
     UserModule,
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+        user: configService.get('MONGODB_USER'),
+        password: configService.get('MONGODB_PASSWORD'),
+
+        connectionFactory: (connection) => {
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, UsersService],
