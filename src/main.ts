489733856c9050a1df45dcd413690f581e08c4a9
fxrcha <truetype24@gmail.com>
2022-09-23 09:58:28 +0900

    feat(app): added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Ingopass 백엔드 문서')
+    .setDescription(
+      '선린인터넷고등학교 전자학생증 서비스 Ingopass의 백엔드 문서입니다.',
+    )
+    .setVersion('0.1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'Token' },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
