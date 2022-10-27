730c38ae0135ef800450f365933ec8264028ac1a
Hyunwoo Cho <truetype24@gmail.com>
2022-10-28 08:54:14 +0900

    chore(strategy): callback production

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -10,7 +10,7 @@ export class IngoStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CALLBACK,
       passReqToCallback: true,
       scope: ['profile', 'email'],
     });
